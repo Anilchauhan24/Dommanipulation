2084e2038765c55dd8f3d84d851fab8d6d375b4a
Anilchauhan10 <bankingidealoan@gmail.com>
2023-10-26 21:05:14 +0530

    update

---
+++ AIRBNB/ImageContainer.js
@@ -0,0 +1,8 @@
+
+const images = [
+    {
+        value: 1536,
+        imgLink: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4mDhmngMCKKowgQGHmGIc-lFT7lOjg1N2zg&usqp=CAU",
+        distance: "10-15 km away",
+    }
+]
---
+++ AIRBNB/airbpnb.js
@@ -0,0 +1,22 @@
+const logo = document.querySelector("h1");
+logo.style.color = "tomato";
+
+const nav = document.querySelector("nav");
+nav.style.display = "flex";
+nav.style.alignItems = "center";
+nav.style.gap = "10px";
+nav.style.justifyContent = "space-between"
+
+const line = document.getElementById("line");
+line.style.width = "100%";
+line.style.height = "1px";
+line.style.backgroundColor = "gray";
+line.style.position = "relative";
+
+
+const links = document.getElementsByClassName("links");
+links.style.display = "flex";
+links.style.wrap = "wrap";
+links.style.gap = "10px";
+links.style.justifyContent = "space-around"
+
